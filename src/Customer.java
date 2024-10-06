b44b2367ff0e3bc83b81078d19596a6cfca0c76e
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2024-10-05 21:33:07 -0700

    Added: getters and setters in Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -15,4 +15,28 @@ public class Customer {
 	public int getCustomerId() {
 		return id;
 	}
+	
+	public Address getAddress() {
+		return this.address;
+	}
+	
+	public Phone getPhone() {
+		return this.phone;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	public void setPhone(Phone phone) {
+		this.phone = phone;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
